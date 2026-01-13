fa6f3cc6f18c00b0437a39ccf7e6d22089a53036
Ahmdshfwn <asaffawan@gmail.com>
2026-01-13 23:00:28 +0700

    Fix: Add missing dashboard,fonts.

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,4 @@
+export default function customerPage()
+{
+    return <div className="font-bold text-xl text-teal-700">This is Page Customers</div>
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,4 @@
+export default function invoicesPage()
+{
+    return <div className="font-bold text-rose-600">This is Invoices Page's</div>
+}
